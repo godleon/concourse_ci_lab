8319626e60a110ee9dea5cfba218946a62f06c0f
godleon <godleon@gmail.com>
2017-01-13 16:32:29 +0800

    environment variable test

--- 03.osp10_tempest/rebuild_pipeline.sh
+++ 03.osp10_tempest/rebuild_pipeline.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+TEAM_NAME="lite"
 PIPELINE_NAME="osp10_tempest"
 
-echo -e "y" | fly -t lite destroy-pipeline -p osp10_tempest
-echo -e "y" | fly -t lite set-pipeline -p osp10_tempest -c pipeline.yml
-fly -t lite unpause-pipeline -p osp10_tempest
+echo -e "y" | fly -t ${TEAM_NAME} destroy-pipeline -p ${PIPELINE_NAME}
+echo -e "y" | fly -t ${TEAM_NAME} set-pipeline -p ${PIPELINE_NAME} -c pipeline.yml
+fly -t lite unpause-pipeline -p ${PIPELINE_NAME}

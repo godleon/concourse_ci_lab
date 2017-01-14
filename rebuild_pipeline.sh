d4a115aabab44bb47566fb57111cee46ce76e511
Leon Tseng <godleon@gmail.com>
2017-01-14 23:25:37 +0800

    simple ansible task - 5

--- rebuild_pipeline.sh
+++ rebuild_pipeline.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-
-
 TEAM_NAME="lite"
 PIPELINE_NAME=${1}
+PIPELINE_FILE=${2}
 
 echo -e "y" | fly -t ${TEAM_NAME} destroy-pipeline -p ${PIPELINE_NAME}
-echo -e "y" | fly -t ${TEAM_NAME} set-pipeline -p ${PIPELINE_NAME} -c pipeline.yml
+echo -e "y" | fly -t ${TEAM_NAME} set-pipeline -p ${PIPELINE_NAME} -c ${PIPELINE_FILE}
 fly -t lite unpause-pipeline -p ${PIPELINE_NAME}

a4e7ad4dcd4a23b8f5d6263c1d402356e0ed8b19
Leon Tseng <godleon@gmail.com>
2017-01-14 23:17:10 +0800

    simple ansible task - 1

--- 03.osp10_tempest/scripts/prepare_env.sh
+++ 03.osp10_tempest/scripts/prepare_env.sh
@@ -2,16 +2,19 @@
 
 source ${1}
 
-subscription-manager register --username ${REDHAT_USERNAME} --password ${REDHAT_USERPASSWORD} --autosubscribe
-subscription-manager attach --pool=${REDHAT_REG_POOLID}
-subscription-manager repos --disable=*
-subscription-manager repos --enable=${REDHAT_REPO_ENABLED}
+mkdir tempest_output
+touch tempest_output/result.txt
 
-yum -y install openstack-tempest
-mkdir /tempest
-cd /tempest
-sh /usr/share/openstack-tempest-13.0.0/tools/configure-tempest-directory
-tools/config_tempest.py --debug --create identity.uri ${OS_AUTH_URL} identity.admin_username ${OS_USERNAME} identity.admin_password ${OS_PASSWORD} identity.admin_tenant_name ${OS_TENANT_NAME} object-storage.operator_role Member
-python -m tempest.cmd.cleanup --init-saved-state
-python -m tempest.cmd.cleanup
-tools/run-tests.sh | tee ra-out.txt
+# subscription-manager register --username ${REDHAT_USERNAME} --password ${REDHAT_USERPASSWORD} --autosubscribe
+# subscription-manager attach --pool=${REDHAT_REG_POOLID}
+# subscription-manager repos --disable=*
+# subscription-manager repos --enable=${REDHAT_REPO_ENABLED}
+
+# yum -y install openstack-tempest
+# mkdir /tempest
+# cd /tempest
+# sh /usr/share/openstack-tempest-13.0.0/tools/configure-tempest-directory
+# tools/config_tempest.py --debug --create identity.uri ${OS_AUTH_URL} identity.admin_username ${OS_USERNAME} identity.admin_password ${OS_PASSWORD} identity.admin_tenant_name ${OS_TENANT_NAME} object-storage.operator_role Member
+# python -m tempest.cmd.cleanup --init-saved-state
+# python -m tempest.cmd.cleanup
+# tools/run-tests.sh | tee ra-out.txt

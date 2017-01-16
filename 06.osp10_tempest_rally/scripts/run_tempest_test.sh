b91a418f9d8600778fb58fe45c517ed844605539
godleon <godleon@gmail.com>
2017-01-16 13:57:49 +0800

    initial input & output test

---
+++ 06.osp10_tempest_rally/scripts/run_tempest_test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source ${1}
+
+touch ./12345
+
+find .
+
+echo "Finised"
+
+# subscription-manager register --username ${REDHAT_USERNAME} --password ${REDHAT_USERPASSWORD} --autosubscribe
+# subscription-manager attach --pool=${REDHAT_REG_POOLID}
+# subscription-manager repos --disable=*
+# subscription-manager repos --enable=${REDHAT_REPO_ENABLED}
+
+# yum -y install openstack-tempest
+# mkdir /tempest
+# cd /tempest
+# sh /usr/share/openstack-tempest-13.0.0/tools/configure-tempest-directory
+# tools/config_tempest.py --debug --create identity.uri ${OS_AUTH_URL} identity.admin_username ${OS_USERNAME} identity.admin_password ${OS_PASSWORD} identity.admin_tenant_name ${OS_TENANT_NAME} object-storage.operator_role Member
+# python -m tempest.cmd.cleanup --init-saved-state
+# python -m tempest.cmd.cleanup
+# tools/run-tests.sh | tee ra-out.txt

5a58375719e52ecba72d2c6bb04b1240a7c92960
godleon <godleon@gmail.com>
2017-01-17 11:13:57 +0800

    slack alert - 20

--- 07.slack_notification/scripts/do_work.sh
+++ 07.slack_notification/scripts/do_work.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pwd
-echo "Hello Slack! ${date}" > osp_test_resource/07.slack_notification/slack_message.txt
+echo "Hello Slack! $(date)" > osp_test_resource/07.slack_notification/slack_message.txt
 find .
 cat osp_test_resource/07.slack_notification/slack_message.txt
 echo "finished"

a027fffe49936e7cf683042e42038be899f77251
godleon <godleon@gmail.com>
2017-01-18 14:31:51 +0800

    slack alert - 1

--- rebuild_pipeline.sh
+++ rebuild_pipeline.sh
@@ -5,5 +5,5 @@ PIPELINE_NAME=${1}
 PIPELINE_FILE=${2}
 
 echo -e "y" | fly -t ${TEAM_NAME} destroy-pipeline -p ${PIPELINE_NAME}
-echo -e "y" | fly -t ${TEAM_NAME} set-pipeline -p ${PIPELINE_NAME} -c ${PIPELINE_FILE}
+echo -e "y" | fly -t ${TEAM_NAME} set-pipeline -p ${PIPELINE_NAME} -c ${PIPELINE_FILE} -n --load-vars-from credentials.yml
 fly -t lite unpause-pipeline -p ${PIPELINE_NAME}

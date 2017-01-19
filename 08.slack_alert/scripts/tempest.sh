6f86b179ea76444d25925b53a4621a807fe8521b
godleon <godleon@gmail.com>
2017-01-19 14:10:55 +0800

    slack alert 4

--- 08.slack_alert/scripts/tempest.sh
+++ 08.slack_alert/scripts/tempest.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "================ in shell script ================"
+source $1
 env
 # echo "Hello Slack! $(date)" > tempest_output/slack_message.txt
 # echo "${BUILD_ID} => in shell script"

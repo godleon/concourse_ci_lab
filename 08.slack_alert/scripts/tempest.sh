2b942eaecdff67d155afa9ef9b3a2a18d204a21d
godleon <godleon@gmail.com>
2017-01-19 15:48:54 +0800

    slack alert 2

--- 08.slack_alert/scripts/tempest.sh
+++ 08.slack_alert/scripts/tempest.sh
@@ -4,6 +4,7 @@ echo "================ in shell script ================"
 echo "OS_AUTH_URL = ${OS_AUTH_URL}"
 source $1
 echo "OS_AUTH_URL = ${OS_AUTH_URL}"
+echo "version number = $(cat $2)"
 # echo "Hello Slack! $(date)" > tempest_output/slack_message.txt
 # echo "${BUILD_ID} => in shell script"
 # find .

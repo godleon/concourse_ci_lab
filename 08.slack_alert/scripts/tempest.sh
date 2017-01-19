d9f884cc036362c62d1d2f930e37bb44af5233a6
godleon <godleon@gmail.com>
2017-01-19 16:19:41 +0800

    slack alert 1

--- 08.slack_alert/scripts/tempest.sh
+++ 08.slack_alert/scripts/tempest.sh
@@ -5,5 +5,5 @@ echo "OS_AUTH_URL = ${OS_AUTH_URL}"
 source $1
 echo "OS_AUTH_URL = ${OS_AUTH_URL}"
 echo "version = $(cat $2)"
-echo "Hello Slack! $(date)" > $3
+echo "Tempest Result Link: https://godleon.github.io/osp_test_results/$(cat $2)/tempest_output.html" > $3
 echo "finished"

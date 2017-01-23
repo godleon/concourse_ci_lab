c2817e1bd5ad57dd0b476f60135d7ee492889488
godleon <godleon@gmail.com>
2017-01-23 11:27:00 +0800

    slack alert - 1

--- 08.slack_alert/scripts/tempest.sh
+++ 08.slack_alert/scripts/tempest.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+set -e # fail fast
+set -x # print commands
+
 echo "================ in shell script ================"
-echo "OS_AUTH_URL = ${OS_AUTH_URL}"
-source $1
-echo "OS_AUTH_URL = ${OS_AUTH_URL}"
-echo "version = $(cat $2)"
-echo "$(date) => Tempest Result Link: https://godleon.github.io/osp_test_results/$(cat $2)/tempest_output.html" > $3
-echo "finished"
+find .
+echo "ANSIBLE_HOST_KEY_CHECKING = ${ANSIBLE_HOST_KEY_CHECKING}"
+echo "Finished!"
+# echo "OS_AUTH_URL = ${OS_AUTH_URL}"
+# source $1
+# echo "OS_AUTH_URL = ${OS_AUTH_URL}"
+# echo "version = $(cat $2)"
+# echo "$(date) => Tempest Result Link: https://godleon.github.io/osp_test_results/$(cat $2)/tempest_output.html" > $3
+# echo "finished"

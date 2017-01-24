d6a2d4b8cbd591e0a09dc471770e0849eb6f7572
godleon <godleon@gmail.com>
2017-01-24 10:28:42 +0800

    tempest ci - 1

--- 08.tempest/scripts/parse_tempest_logs.sh
+++ 08.tempest/scripts/parse_tempest_logs.sh
@@ -10,17 +10,29 @@ find .
 apt-get update
 apt-get -y install git python3 python3-jinja2
 
-git clone git-resource_build tempest_parsed_result
-
 cp git-resource_osp/08.tempest/scripts/{template.html,tempest_parser.py} ./
-cp tempest_parsed_result/$(cat git-resource_semver/version)/ra-out.txt ./
-rm -rf tempest_parsed_result/$(cat git-resource_semver/version)
+cp git-resource_build/$(cat git-resource_semver/version)/ra-out.txt ./
 python3 tempest_parser.py
-
-find .
-
 git clone git-resource_test-result tempest_parsed_result
 mkdir -p tempest_parsed_result/$(cat git-resource_semver/version)
 cp tempest_output.html ra-out.txt tempest_parsed_result/$(cat git-resource_semver/version)/tempest.html
+
+cd tempest_parsed_result
+git config --global user.email "nobody@concourse.ci"
+git config --global user.name "Concourse"
+git add .
+git commit -m "Tempest output version $(cat git-resource_semver/version)"
+cd -
+
+git clone git-resource_build git-resource_build_clean
+rm -rf git-resource_build_clean/$(cat git-resource_semver/version)
+cd git-resource_build_clean
+git config --global user.email "nobody@concourse.ci"
+git config --global user.name "Concourse"
+git add .
+git commit -m "Clean Tempest Build version $(cat git-resource_semver/version)"
+cd -
+
+find .
 
 echo "Parse Finished!"
--- 08.tempest/scripts/run_tempest.sh
+++ 08.tempest/scripts/run_tempest.sh
@@ -5,35 +5,32 @@ set -x # print commands
 
 echo "================ in shell script (run_tempest.sh) ================"
 
-# subscription-manager register --username ${REDHAT-USER_NAME} --password ${REDHAT-USER_PWD} --autosubscribe
-# subscription-manager attach --pool=${REDHAT-REG_POOLID}
-# subscription-manager repos --disable=*
-# subscription-manager repos --enable=${REDHAT-REPO_ENABLED}
+subscription-manager register --username ${REDHAT_USER_NAME} --password ${REDHAT_USER_PWD} --autosubscribe
+subscription-manager attach --pool=${REDHAT_REG_POOLID}
+subscription-manager repos --disable=*
+subscription-manager repos --enable=${REDHAT_REPO_ENABLED}
 
 env
 find .
 echo "ANSIBLE_HOST_KEY_CHECKING = ${ANSIBLE_HOST_KEY_CHECKING}"
 echo "REDHAT_USER_NAME = ${REDHAT_USER_NAME}"
-echo "Finished!"
 
+yum -y install git
+git clone git-resource_build tempest_output
+cat git-resource_semver/version
+mkdir -p tempest_output/$(cat git-resource_semver/version)
+cp git-resource_osp/08.tempest/raw_logs/ra-out.txt tempest_output/$(cat git-resource_semver/version)/
+#cp git-resource_osp/08.tempest/scripts/tempest.xz tempest_output/$(cat git-resource_semver/version)/
 
+cd tempest_output
+git config --global user.email "nobody@concourse.ci"
+git config --global user.name "Concourse"
+git add .
+git commit -m "Tempest raw output version $(cat git-resource_semver/version)"
 
-# yum -y install git
-# git clone git-resource_build tempest_output
-# cat git-resource_semver/version
-# mkdir -p tempest_output/$(cat git-resource_semver/version)
-# cp git-resource_osp/08.tempest/raw_logs/ra-out.txt tempest_output/$(cat git-resource_semver/version)/
-# #cp git-resource_osp/08.tempest/scripts/tempest.xz tempest_output/$(cat git-resource_semver/version)/
-
-# cd tempest_output
-# git config --global user.email "nobody@concourse.ci"
-# git config --global user.name "Concourse"
-# git add .
-# git commit -m "Tempest output version $(cat git-resource_semver/version)"
-
-# echo "OS_AUTH_URL = ${OS_AUTH_URL}"
-# source $1
-# echo "OS_AUTH_URL = ${OS_AUTH_URL}"
-# echo "version = $(cat $2)"
-# echo "$(date) => Tempest Result Link: https://godleon.github.io/osp_test_results/$(cat $2)/tempest_output.html" > $3
-# echo "finished"
+echo "OS_AUTH_URL = ${OS_AUTH_URL}"
+source $1
+echo "OS_AUTH_URL = ${OS_AUTH_URL}"
+echo "version = $(cat $2)"
+echo "$(date) => Tempest Result Link: https://godleon.github.io/osp_test_results/$(cat $2)/tempest_output.html" > $3
+echo "finished"

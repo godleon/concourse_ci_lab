b99c4da69ee9db65e73fa9db24e38f3f4584db0d
godleon <godleon@gmail.com>
2017-01-24 10:48:51 +0800

    tempest ci - 2

--- 08.tempest/scripts/run_tempest.sh
+++ 08.tempest/scripts/run_tempest.sh
@@ -5,6 +5,8 @@ set -x # print commands
 
 echo "================ in shell script (run_tempest.sh) ================"
 
+RESOURCE_VER=$(cat git-resource_semver/version)
+
 subscription-manager register --username ${REDHAT_USER_NAME} --password ${REDHAT_USER_PWD} --autosubscribe
 subscription-manager attach --pool=${REDHAT_REG_POOLID}
 subscription-manager repos --disable=*
@@ -19,18 +21,18 @@ yum -y install git
 git clone git-resource_build tempest_output
 cat git-resource_semver/version
 mkdir -p tempest_output/$(cat git-resource_semver/version)
-cp git-resource_osp/08.tempest/raw_logs/ra-out.txt tempest_output/$(cat git-resource_semver/version)/
+cp git-resource_osp/08.tempest/raw_logs/ra-out.txt tempest_output/${RESOURCE_VER}/
 #cp git-resource_osp/08.tempest/scripts/tempest.xz tempest_output/$(cat git-resource_semver/version)/
 
 cd tempest_output
 git config --global user.email "nobody@concourse.ci"
 git config --global user.name "Concourse"
 git add .
-git commit -m "Tempest raw output version $(cat git-resource_semver/version)"
+git commit -m "Tempest raw output version ${RESOURCE_VER}"
 
-echo "OS_AUTH_URL = ${OS_AUTH_URL}"
-source $1
-echo "OS_AUTH_URL = ${OS_AUTH_URL}"
-echo "version = $(cat $2)"
-echo "$(date) => Tempest Result Link: https://godleon.github.io/osp_test_results/$(cat $2)/tempest_output.html" > $3
-echo "finished"
+# echo "OS_AUTH_URL = ${OS_AUTH_URL}"
+# source $1
+# echo "OS_AUTH_URL = ${OS_AUTH_URL}"
+# echo "version = $(cat $2)"
+# echo "$(date) => Tempest Result Link: https://godleon.github.io/osp_test_results/$(cat $2)/tempest_output.html" > $3
+# echo "finished"

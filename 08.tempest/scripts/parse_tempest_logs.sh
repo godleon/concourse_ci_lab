9774dd3853b82466c8dddf9260860eff2a34c1ed
godleon <godleon@gmail.com>
2017-01-24 13:18:04 +0800

    tempest ci - 2

--- 08.tempest/scripts/parse_tempest_logs.sh
+++ 08.tempest/scripts/parse_tempest_logs.sh
@@ -4,6 +4,9 @@ set -e # fail fast
 set -x # print commands
 
 echo "================ in shell script (parse_tempest_logs.sh) ================"
+
+RESOURCE_VER=$(cat git-resource_semver/version)
+
 env
 find .
 
@@ -11,26 +14,37 @@ apt-get update
 apt-get -y install git python3 python3-jinja2
 
 cp git-resource_osp/08.tempest/scripts/{template.html,tempest_parser.py} ./
-cp git-resource_build/$(cat git-resource_semver/version)/ra-out.txt ./
+cp git-resource_build/${RESOURCE_VER}/ra-out.txt ./
 python3 tempest_parser.py
-git clone git-resource_test-result tempest_parsed_result
-mkdir -p tempest_parsed_result/$(cat git-resource_semver/version)
-cp tempest_output.html ra-out.txt tempest_parsed_result/$(cat git-resource_semver/version)/tempest.html
 
+git clone git-resource_test-result tempest_parsed_result
+
+if [ -d tempest_parsed_result/${RESOURCE_VER} ]; then
+    rm -rf tempest_parsed_result/${RESOURCE_VER}
+    cd tempest_output
+    git config --global user.email "nobody@concourse.ci"
+    git config --global user.name "Concourse"
+    git add -A
+    git commit -m "Clean garbage file version ${RESOURCE_VER}"
+    cd -
+fi
+
+mkdir -p tempest_parsed_result/${RESOURCE_VER}
+cp tempest_output.html ra-out.txt tempest_parsed_result/${RESOURCE_VER}/
 cd tempest_parsed_result
 git config --global user.email "nobody@concourse.ci"
 git config --global user.name "Concourse"
 git add .
-git commit -m "Tempest output version $(cat git-resource_semver/version)"
+git commit -m "Tempest output version ${RESOURCE_VER}"
 cd -
 
 git clone git-resource_build git-resource_build_clean
-rm -rf git-resource_build_clean/$(cat git-resource_semver/version)
+rm -rf git-resource_build_clean/${RESOURCE_VER}
 cd git-resource_build_clean
 git config --global user.email "nobody@concourse.ci"
 git config --global user.name "Concourse"
 git add .
-git commit -m "Clean Tempest Build version $(cat git-resource_semver/version)"
+git commit -m "Clean Tempest Build version ${RESOURCE_VER}"
 cd -
 
 find .

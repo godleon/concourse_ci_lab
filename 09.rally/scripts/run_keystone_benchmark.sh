66fb08dbb3db8818cc5d028b2977c9326f543cc5
godleon <godleon@gmail.com>
2017-02-07 14:39:31 +0800

    rally test 3

--- 09.rally/scripts/run_keystone_benchmark.sh
+++ 09.rally/scripts/run_keystone_benchmark.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+set -e # fail fast
+set -x # print commands
+
 echo "In scripts/run_keystone_benchmark.sh"
 
 rally-manage db recreate
 
 rally deployment create --fromenv --name=existing
+
+source ~/.rally/openrc
 
 rally deployment check
 

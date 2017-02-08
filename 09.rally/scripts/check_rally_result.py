262aa8e01930eb5d722428a7e98ae55963df7f0e
godleon <godleon@gmail.com>
2017-02-08 11:26:54 +0800

    rally test 8

--- 09.rally/scripts/check_rally_result.py
+++ 09.rally/scripts/check_rally_result.py
@@ -1,8 +1,11 @@
-import json
+import os, json
+
+cur_path = os.path.dirname(os.path.abspath(__file__))
+result_path = cur_path + './rally_result.json'
 
 data = []
 
-with open('rally_result.json', 'r') as f:
+with open(result_path, 'r') as f:
     data = json.load(f)
 
 print(data[0]['sla'][len(data[0]['sla']) - 1]['success'])

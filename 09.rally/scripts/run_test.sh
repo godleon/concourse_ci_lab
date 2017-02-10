61ac7f170f437e4f28e6fc07ac79a83993648834
godleon <godleon@gmail.com>
2017-02-10 10:19:11 +0800

    rally test 2

--- 09.rally/scripts/run_test.sh
+++ 09.rally/scripts/run_test.sh
@@ -3,6 +3,17 @@
 set -e # fail fast
 set -x # print commands
 
+ENTRY_PATH=$(pwd)
+RESOURCE_VER=$(cat git_semver/version)
+
+whoami
+env
+
+sudo apt-get update
+sudo apt-get -y install git
+
+cd ${HOME}
+
 rally-manage db recreate
 rally deployment create --fromenv --name=existing
 rally deployment check
@@ -10,9 +21,9 @@ rally deployment check
 openstack image list
 openstack flavor list
 
-REMAIN_TEST_COUNT=$(find rally_test/ -type f -name '*.json' | wc -l)
+REMAIN_TEST_COUNT=$(find ${ENTRY_PATH}/rally_test/ -type f -name '*.json' | wc -l)
 
-CUR_TEST=$(find /home/godleon/Dropbox/Develop/CI-CD/concourse_ci_lab/09.rally/scripts/propagated_tests -type f -name '*.json' | sort | head -1)
+CUR_TEST=$(find ${ENTRY_PATH}/rally_test/ -type f -name '*.json' | sort | head -1)
 
 env
 
@@ -21,4 +32,4 @@ env
 # find .
 # sudo rally task results | sudo tee rally_result.json
 
-find .
+find ${ENTRY_PATH}

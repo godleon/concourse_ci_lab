2a23a88334e2e842f2cac5babdba3c5bb1c833d9
godleon <godleon@gmail.com>
2017-02-14 18:29:11 +0800

    tempest test RA

--- rebuild_pipeline.sh
+++ rebuild_pipeline.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-TEAM_NAME="lite"
-PIPELINE_NAME=${1}
-PIPELINE_FILE=${2}
+TEAM_NAME="osp10"
+CREDENTIAL_FILE=${1}
+PIPELINE_NAME=${2}
+PIPELINE_FILE=${3}
 
 echo -e "y" | fly -t ${TEAM_NAME} destroy-pipeline -p ${PIPELINE_NAME}
-echo -e "y" | fly -t ${TEAM_NAME} set-pipeline -p ${PIPELINE_NAME} -c ${PIPELINE_FILE} -n --load-vars-from credentials.yml
+echo -e "y" | fly -t ${TEAM_NAME} set-pipeline -p ${PIPELINE_NAME} -c ${PIPELINE_FILE} -n --load-vars-from ${CREDENTIAL_FILE}
 fly -t lite unpause-pipeline -p ${PIPELINE_NAME}
